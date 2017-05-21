340539f4a3b9bca7c8160f50e8e21d9b82cb2592
Giordano Alves <giordanofalves@gmail.com>
2017-05-21 18:02:16 +0100

    (#9 fix) Remove state from autocomplete selected value.

--- app/controllers/locate_controller.rb
+++ app/controllers/locate_controller.rb
@@ -5,8 +5,17 @@ class LocateController < ApplicationController
 
     respond_to do |format|
       format.json do
-        render json: cities['predictions'].map { |city| city['description'] }
+        render json: cities['predictions'].map { |city|
+          { label: city['description'],
+            value: autocomplete_value(city['terms']) }
+        }
       end
     end
+  end
+
+  private
+
+  def autocomplete_value(terms)
+    "#{terms.first['value']}, #{terms.last['value']}"
   end
 end
